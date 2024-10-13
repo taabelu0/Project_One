0c5b605532b241269fe9a17ab6d10433757bd23a
taabelu0 <luca.bertonazzi@students.fhnw.ch>
2024-10-13 20:03:54 +0200

    Initial Commit

---
+++ Project_One/Project_OneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_OneApp.swift
+//  Project_One
+//
+//  Created by Luca Bertonazzi on 13.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct Project_OneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
